3ed0ffede3395e1b75685357c30c9fc12c8d1595
MARTIN-TAMIMOUNT Yamina <yamina.martintamimount@hotmail.com>
2026-04-20 16:52:54 +0200

    premier commit

---
+++ src/main/java/Bief/Spring/Boot/creer/une/application/Hello/World/HelloWorldApplication.java
@@ -0,0 +1,19 @@
+package Bief.Spring.Boot.creer.une.application.Hello.World;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello Word");
+	}
+
+}
---
+++ src/test/java/Bief/Spring/Boot/creer/une/application/Hello/World/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package Bief.Spring.Boot.creer.une.application.Hello.World;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
